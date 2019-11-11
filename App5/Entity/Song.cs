d25c23bc004f259a48641efa133ff100d56649da
khaihqth1807028 <khaihqth1807028@fpt.edu.vn>
2019-11-11 13:01:06 +0700

    lam bai tap

---
+++ App5/Entity/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App5.Entity
+{
+    class Song
+    {
+        public string name { get; set; }
+        public string singer { get; set; }
+        public string thumbnail { get; set; }
+        public string link { get; set; }
+    }
+}
+
---
+++ App5/Pages/LoginPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using App5.Entity;
+using App5.Service;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App5.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginPage : Windows.UI.Xaml.Controls.Page
+    {
+
+        private IMemberService _memberService;
+        private IFileService _fileService;
+
+        public LoginPage()
+        {
+            Debug.WriteLine("Init login.");
+            this.InitializeComponent();
+            this._memberService = new MemberService();
+            this._fileService = new LocalFileService();
+        }
+
+
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(RegisterPage));
+        }
+
+        private void ButtonLogin_OnClick(object sender, RoutedEventArgs e)
+        {
+            var memberLogin = new MemberLogin
+            {
+                email = Email.Text,
+                password = Password.Password
+            };
+            var memberCredential = this._memberService.Login(memberLogin);
+            this._fileService.SaveMemberCredentialToFile(memberCredential);
+        }
+
+    }
+}
